350509158fd755ecb9f8f0c48170f5c6520c8b2d
Nachiket <waghambare.nachiket@gmail.com>
2022-07-27 05:59:26 +0530

    Day 1 Band Name Generator

---
+++ Day1_BandNameGenerator/main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 27 05:52:01 2022
+
+@author: admin
+"""
+city_name = input('Where were you born?\n')
+pet_name = input('What is the name of your pet?\n')
+
+band_name = city_name + ' ' + pet_name
+
+print('Your band name can be {}'.format(band_name))
